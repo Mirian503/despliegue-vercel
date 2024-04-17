7f429e753bb2005a8d3fd02a91bf595e28c6195a
Mirian Ocampo <ocampomirian74@gmail.com>
2024-04-17 09:10:03 -1200

    coment environ

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from dotenv import load_dotenv
+"""from dotenv import load_dotenv
 from os import environ
 load_dotenv ()
-
+"""
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
